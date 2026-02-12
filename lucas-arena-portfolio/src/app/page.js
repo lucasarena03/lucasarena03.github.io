7c9c3f20ea2c5232561adfea171fd0c11845d95d
Lucas Arena <lucasa4585@gmail.com>
2026-02-11 23:05:33 -0500

    Added portfilio picture and finished structure for Experience, Education, Projects and Contact sections

--- lucas-arena-portfolio/src/app/page.js
+++ lucas-arena-portfolio/src/app/page.js
@@ -1,16 +1,17 @@
-import { Box, Container, List, ListItem, Skeleton, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, List, ListItem, Paper, Skeleton, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <Container maxWidth="md" sx={{ display: "flex", justifyContent: "center", alignItems: "flex-start", minHeight: "100vh" }}>
-      <Box sx={{ mx: 4, mt: 8, width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        {/* Picture and intro section */}
-        <Box sx={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-          <Skeleton variant="circular" width={300} height={300} />
+      <Paper elevation={1} sx={{ p: 4, mt: 2, width: "100%", display: "flex", flexDirection: "column", alignItems: "center", borderRadius: 8 }}>
+        {/* Picture and intro section - will make a component soon */}
+        <Box sx={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Image src="/portfolio.png" alt="Portfolio" width={400} height={400} style={{ borderRadius: "40%" }} />
           <Box sx={{ mt: 4, width: "100%", textAlign: "center" }}>
             <Typography variant="h3">Lucas Arena</Typography>
-            <Typography variant="h5" sx={{ mt: 2 }}>IT Specialist & Developer</Typography>
+            <Typography variant="h5" sx={{ mt: 2, fontWeight: "bold" }}>IT Specialist & Developer</Typography>
           </Box>
           <Box sx={{ mt: 4 }}>
             <Typography variant="body1" sx={{ mb: 2 }}>
@@ -18,27 +19,77 @@ const Home = () => {
             </Typography>
           </Box>
         </Box>
-      {/* Experience */}
+        {/* Experience - will make a component soon */}
         <Box sx={{ width: "100%", mt: 6 }}>
-          <Typography variant="h4" sx={{ mb: 2 }}>Experience</Typography>
+          <Typography variant="h4" sx={{ mb: 2, textDecoration: "underline" }}>Experience</Typography>
           <Box sx={{ mb: 4 }}>
-            <Typography variant="h6">Computer Lab Tech</Typography>
-            <Typography variant="body2" color="text.secondary">Start Date - End Date</Typography>
-            <List sx={{}}>
-              <ListItem>
-                <Typography variant="body1">
-                  Did some stuff in a lab. Expand upon this Luke idk what else to put here.
-                </Typography>
+            <Typography variant="h5">Florida State University</Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+              <Typography variant="body1">Computer Lab Tech</Typography>
+              <Typography variant="body2" color="text.secondary">Start Date - End Date</Typography>
+            </Box>
+            <List sx={{ listStyleType: "square", pl: 4 }}>
+              <ListItem sx={{ display: "list-item" }}>
+                Job duties and stuff
               </ListItem>
-              <ListItem>
-                <Typography variant="body1">
-                  Put more bullet points.
-                </Typography>
+              <ListItem sx={{ display: "list-item" }}>
+                Put more bullet points
               </ListItem>
             </List>
           </Box>
         </Box>
-      </Box>
+        <Divider orientation="horizontal" flexItem />
+        {/* Education - will make a component soon */}
+        <Box sx={{ width: "100%", mt: 4 }}>
+          <Typography variant="h4" sx={{ mb: 2, textDecoration: "underline" }}>Education</Typography>
+          <Box sx={{ mb: 4 }}>
+            <Typography variant="h5">Florida Gulf Coast University</Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+              <Typography variant="body1">B.S. in Computer Information Systems</Typography>
+              <Typography variant="body2" color="text.secondary">Start Date - End Date</Typography>
+            </Box>
+            <List sx={{ listStyleType: "square", pl: 4 }}>
+              <ListItem sx={{ display: "list-item" }}>
+                Any awards or other (e.g. President's List)
+              </ListItem>
+            </List>
+          </Box>
+        </Box>
+        <Divider orientation="horizontal" flexItem />
+        <Box sx={{ width: "100%", mt: 4, display: "flex", flexDirection: "column" }}>
+          <Typography variant="h4" sx={{ mb: 2, textDecoration: "underline" }}>Projects</Typography>
+          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", my: 4 }}>
+            <Typography variant="h5">Project Title</Typography>
+            <Skeleton variant="rectangular" width={300} height={300} sx={{ my: 2 }} />
+            <Typography variant="body1">Description of the project goes here</Typography>
+            <Button variant="outlined" color="primary" sx={{ mt: 2 }}>Source Code</Button>
+            <Button variant="contained" color="primary" sx={{ mt: 2 }}>View Project</Button>
+          </Box>
+          <Divider orientation="horizontal" flexItem />
+          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", my: 4 }}>
+            <Typography variant="h5">Project Title</Typography>
+            <Skeleton variant="rectangular" width={300} height={300} sx={{ my: 2 }} />
+            <Typography variant="body1">Description of the project goes here</Typography>
+            <Button variant="outlined" color="primary" sx={{ mt: 2 }}>Source Code</Button>
+            <Button variant="contained" color="primary" sx={{ mt: 2 }}>View Project</Button>
+          </Box>
+        </Box>
+        <Divider orientation="horizontal" flexItem />
+        <Box sx={{ width: "100%", mt: 4 }}>
+          <Typography variant="h4" sx={{ mb: 8, textDecoration: "underline" }}>Contact</Typography>
+          <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "stretch" }}>
+            <Link href="mailto:lucasa4585@gmail.com" passHref>
+              <Button variant="contained" color="primary" sx={{ fontSize: 24 }}>Email</Button>
+            </Link>
+            <Link href="https://github.com/lucasarena03" passHref>
+              <Button variant="contained" color="github" sx={{ fontSize: 24 }}>GitHub</Button>
+            </Link>
+            <Link href="https://www.linkedin.com/in/lucas-arena-0a9239249/" passHref>
+              <Button variant="contained" color="linkedin" sx={{ fontSize: 24 }}>LinkedIn</Button>
+            </Link>
+          </Box>
+        </Box>
+      </Paper>
     </Container>
   );
 };
--- lucas-arena-portfolio/src/app/theme.js
+++ lucas-arena-portfolio/src/app/theme.js
@@ -1,8 +1,14 @@
 const { createTheme } = require("@mui/material");
 
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (color) => augmentColor({ color: { main: color }});
+
 const theme = createTheme({
     palette: {
         mode: 'dark',
+        github: createColor("#333333"),
+        linkedin: createColor("#0A66C2"),
     }
 });
 
