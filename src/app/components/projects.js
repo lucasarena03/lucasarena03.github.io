24786efeccac2769f1d6c88f42862dd5fdc894ba
Devin Arena <devinarena03@gmail.com>
2026-02-23 23:28:12 -0600

    Football Game project

--- src/app/components/projects.js
+++ src/app/components/projects.js
@@ -1,13 +1,15 @@
-import { Box, Button, Divider, Skeleton, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
-const ProjectSection = ({ title, description }) => {
+const ProjectSection = ({ title, description, imageSrc, imageAlt, sourceCodeLink, projectLink }) => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", my: 4 }}>
       <Typography variant="h5">{title}</Typography>
-      <Skeleton variant="rectangular" width={300} height={300} sx={{ my: 2 }} />
-      <Typography variant="body1">{description}</Typography>
-      <Button variant="outlined" color="primary" sx={{ mt: 2 }}>Source Code</Button>
-      <Button variant="contained" color="primary" sx={{ mt: 2 }}>View Project</Button>
+      <Image src={imageSrc} alt={imageAlt} width={718} height={381} style={{ borderRadius: 8, marginTop: 16, marginBottom: 16 }} />
+      <Typography variant="body1" sx={{}}>{description}</Typography>
+      <Link href={sourceCodeLink} passHref><Button variant="outlined" color="primary" sx={{ mt: 2 }}>Source Code</Button></Link>
+      <Link href={projectLink} passHref><Button variant="contained" color="primary" sx={{ mt: 2 }}>View Project</Button></Link>
     </Box>
   );
 }
@@ -16,9 +18,14 @@ const Projects = () => {
   return (
     <Box sx={{ width: "100%", mt: 4, display: "flex", flexDirection: "column" }}>
       <Typography variant="h4" sx={{ mb: 2, textDecoration: "underline" }}>Projects</Typography>
-      <ProjectSection title="Project 1" description="Description of project 1 goes here." />
-      <Divider orientation="horizontal" flexItem />
-      <ProjectSection title="Project 2" description="Description of project 2 goes here." />
+      <ProjectSection
+        title="Football Game"
+        description="A fast-paced arcade-style game where you grab the ball, dodge relentless defenders, and race for the end zone to score as many touchdowns as possible before you're caught. With an escalating challenge, each run tests your reflexes and movement skills while chasing a new high score. Built with the Godot Engine, it runs smoothly on both PC and Android. Unofficially released, but a v0.2 debug APK provided on GitHub."
+        imageSrc="/football_game.png"
+        imageAlt="Football Game"
+        sourceCodeLink="https://github.com/lucasarena03/arcade-game"
+        projectLink="https://github.com/lucasarena03/arcade-game/blob/main/Football%20Game.apk"
+      />
     </Box>
   );
 }
